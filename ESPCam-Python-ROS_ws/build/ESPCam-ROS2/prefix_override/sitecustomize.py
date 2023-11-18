05b5c305e1961a2e799ecac732d9ed6b05b86703
Devesh Nath <deveshnath101@gmail.com>
2023-11-17 23:24:20 -0700

    Try

---
+++ ESPCam-Python-ROS_ws/build/ESPCam-ROS2/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/mnt/c/Users/deves/Desktop/Desktop All/Projects/Capstone/Capstone-Autonomous-Driving/ESPCam-Python-ROS_ws/install/ESPCam-ROS2'
