8bf19224a762ba04652a5b94795699b0a58f95a7
Devesh Nath <deveshnath101@gmail.com>
2023-11-19 09:29:29 -0700

    Testing
    Camera config functions

--- ESPCam-Python-ROS_ws/src/cv_basics/cv_basics/webcam_pub.py
+++ ESPCam-Python-ROS_ws/src/cv_basics/cv_basics/webcam_pub.py
@@ -5,6 +5,7 @@ from rclpy.node import Node # Handles the creation of nodes
 from sensor_msgs.msg import Image # Image is the message type
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 import cv2 # OpenCV library
+import requests
 
 class ImagePublisher(Node):
 
@@ -15,7 +16,11 @@ class ImagePublisher(Node):
     self.publisher_ = self.create_publisher(Image, 'video_frames', 10)
     timer_period = 0.1  # seconds
     self.timer = self.create_timer(timer_period, self.timer_callback)
-         
+
+    self.set_resolution(self.URL, 7)  
+    self.set_quality(self.URL, 4) 
+    self.set_flash(self.URL, 255)
+    self.set_awb(self.URL, 1)
     self.cap = cv2.VideoCapture(self.URL + ":81/stream")
     self.br = CvBridge()
    
@@ -25,6 +30,19 @@ class ImagePublisher(Node):
         self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
 
     self.get_logger().info('Publishing video frame')
+
+  '''Resuolutions: 10: UXGA(1600x1200) 9: SXGA(1280x1024) 8: XGA(1024x768) 7: SVGA(800x600) 6: VGA(640x480) 5: CIF(400x296) 4: QVGA(320x240) 3: HQVGA(240x176) 0: QQVGA(160x120)'''
+  def set_resolution(url: str, index: int=7):
+      requests.get(url + "/control?var=framesize&val={}".format(index))
+
+  def set_quality(url: str, value: int=4):
+    requests.get(url + "/control?var=quality&val={}".format(value))
+  
+  def set_awb(url: str, awb: int=1):
+    requests.get(url + "/control?var=awb&val={}".format(awb))
+
+  def set_flash(url: str, intensity: int=255):
+      requests.get(url + "/control?var=led_intensity&val={}".format(intensity))
   
 def main(args=None):
   rclpy.init(args=args)
