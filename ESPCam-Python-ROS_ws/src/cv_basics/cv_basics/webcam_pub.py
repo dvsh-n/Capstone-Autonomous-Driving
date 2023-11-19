290ea65b481f76c6802e710a502f7c1c6b8c4285
Devesh Nath <deveshnath101@gmail.com>
2023-11-18 19:06:34 -0700

    webcam sub and pub added

---
+++ ESPCam-Python-ROS_ws/src/cv_basics/cv_basics/webcam_pub.py
@@ -0,0 +1,37 @@
+
+# Import the necessary libraries
+import rclpy # Python Client Library for ROS 2
+from rclpy.node import Node # Handles the creation of nodes
+from sensor_msgs.msg import Image # Image is the message type
+from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
+import cv2 # OpenCV library
+import requests # For comms with ESP32 Cam
+
+class ImagePublisher(Node):
+
+  def __init__(self):
+    super().__init__('image_publisher')
+    self.URL = "http://192.168.0.102"
+    self.publisher_ = self.create_publisher(Image, 'video_frames', 10)
+    timer_period = 0.1  # seconds
+    self.timer = self.create_timer(timer_period, self.timer_callback)
+         
+    self.cap = cv2.VideoCapture(self.URL + ":81/stream")
+    self.br = CvBridge()
+   
+  def timer_callback(self):
+    ret, frame = self.cap.read()   
+    if ret == True:
+        self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
+
+    self.get_logger().info('Publishing video frame')
+  
+def main(args=None):
+  rclpy.init(args=args)
+  image_publisher = ImagePublisher()
+  rclpy.spin(image_publisher)
+  image_publisher.destroy_node()
+  rclpy.shutdown()
+  
+if __name__ == '__main__':
+  main()
