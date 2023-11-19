21ad33365014db78075cbfd0a734f64efa4f2b4c
Devesh Nath <deveshnath101@gmail.com>
2023-11-19 09:38:09 -0700

    Testing
    Camera Config functions

--- ESPCam-Python-ROS_ws/src/cv_basics/cv_basics/webcam_pub.py
+++ ESPCam-Python-ROS_ws/src/cv_basics/cv_basics/webcam_pub.py
@@ -17,7 +17,7 @@ class ImagePublisher(Node):
     timer_period = 0.1  # seconds
     self.timer = self.create_timer(timer_period, self.timer_callback)
 
-    self.set_resolution(7)  
+    self.set_resolution(10)  
     self.set_quality(4) 
     self.set_flash(255)
     self.set_awb(1)
