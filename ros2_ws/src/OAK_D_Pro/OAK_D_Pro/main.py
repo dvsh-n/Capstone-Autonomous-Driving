40463f041f619c92cf330b8f8b9f1e0b2be3826e
Devesh Nath <deveshnath101@gmail.com>
2024-02-04 13:30:03 -0700

    OAK D Pro ros node, to be tested

--- ros2_ws/src/OAK_D_Pro/OAK_D_Pro/main.py
+++ ros2_ws/src/OAK_D_Pro/OAK_D_Pro/main.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float64MultiArray
+from sensor_msgs.msg import Image # Image is the message type
+from geometry_msgs.msg import Quaternion
+from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
+from datetime import timedelta
+import cv2 # OpenCV library
 import depthai as dai
 import numpy as np
 
-class Depth_Frame_Pub(Node):
+class OAK_D_Pro(Node):
 
     def __init__(self):
-        super().__init__('depth_publisher')
-        self.publisher = self.create_publisher(Float64MultiArray, 'OAK_D_Pro/depth', 10)
+        super().__init__('OAK_D_Pro')
+        self.pub_depth = self.create_publisher(Image, 'OAK_D_Pro/depth', 10)
+        self.pub_color = self.create_publisher(Image, 'OAK_D_Pro/color', 10)
+        self.pub_quat = self.create_publisher(Quaternion, 'OAK_D_Pro/IMU/Quaternion', 10)
         self.pipeline, self.depth  = self.create_pipeline()
+        self.cv_bridge = CvBridge()
 
     def create_pipeline(self):
         # Closer-in minimum depth, disparity range is doubled (from 95 to 190):
@@ -27,9 +34,14 @@ class Depth_Frame_Pub(Node):
         monoLeft = pipeline.create(dai.node.MonoCamera)
         monoRight = pipeline.create(dai.node.MonoCamera)
         depth = pipeline.create(dai.node.StereoDepth)
-        xout = pipeline.create(dai.node.XLinkOut)
+        color = pipeline.create(dai.node.ColorCamera)
+        imu = pipeline.create(dai.node.IMU)
+        sync = pipeline.create(dai.node.Sync)
+        xoutImu = pipeline.create(dai.node.XLinkOut)
+        xoutImu.setStreamName("imu")
 
-        xout.setStreamName("disparity")
+        xoutGrp = pipeline.create(dai.node.XLinkOut)
+        xoutGrp.setStreamName("xout")
 
         # Properties
         monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
@@ -37,49 +49,75 @@ class Depth_Frame_Pub(Node):
         monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
         monoRight.setCamera("right")
 
-        # Create a node that will produce the depth map (using disparity output as it's easier to visualize depth this way)
-        depth.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
-        # Options: MEDIAN_OFF, KERNEL_3x3, KERNEL_5x5, KERNEL_7x7 (default)
-        depth.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
+        color.setCamera("color")
+        color.setResolution(dai.ColorCameraProperties.SensorResolution.THE_720_P)
+
+        imu.enableIMUSensor(dai.IMUSensor.ROTATION_VECTOR, 120)
+        imu.setBatchReportThreshold(1)
+        imu.setMaxBatchReports(10)
+        
+        depth.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY) # Create a node that will produce the depth map (using disparity output as it's easier to visualize depth this way)
+        depth.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7) # Options: MEDIAN_OFF, KERNEL_3x3, KERNEL_5x5, KERNEL_7x7 (default)
         depth.setLeftRightCheck(lr_check)
         depth.setExtendedDisparity(extended_disparity)
         depth.setSubpixel(subpixel)
 
+        sync.setSyncThreshold(timedelta(milliseconds=10))
+        sync.setSyncAttempts(-1)
+
         # Linking
         monoLeft.out.link(depth.left)
         monoRight.out.link(depth.right)
-        depth.disparity.link(xout.input)
+
+        depth.disparity.link(sync.inputs["depth"])
+        imu.out.link(sync.inputs["imu"])
+        color.video.link(sync.inputs["video"])
+
+        sync.out.link(xoutGrp.input)
     
         return pipeline, depth
 
     def run(self):
-        frame_msg = Float64MultiArray()
         # Connect to device and start pipeline
         device = dai.Device(self.pipeline)
 
         # Output queue will be used to get the disparity frames from the outputs defined above
-        q = device.getOutputQueue(name="disparity", maxSize=4, blocking=False)
+        groupQueue = device.getOutputQueue("xout", maxSize=10, blocking=False)
 
         # IR LED Illumination and IR Dot Projector
         device.setIrLaserDotProjectorIntensity(1)
         device.setIrFloodLightIntensity(1)
 
         while rclpy.ok():
-            inDisparity = q.get()  # blocking call, will wait until a new data has arrived
-            frame = inDisparity.getFrame()
-            frame_msg.data = np.array(frame).flatten().astype(np.float32)
-            self.publisher.publish(frame_msg)
-            self.get_logger().info('Publishing Frame')
-            
+            groupMessage = groupQueue.get()
+            inDisparity = groupMessage["depth"]
+            imuMessage = groupMessage["imu"]
+            inCamera = groupMessage["video"]
+
+            frame_depth = inDisparity.getFrame()
+            frame_depth = (frame_depth * (255 / self.depth.initialConfig.getMaxDisparity())).astype(np.uint8) # Normalization for better visualization
+            # cv2.imshow("disparity", frame_depth) # B/W 
+            frame_depth = cv2.applyColorMap(frame_depth, cv2.COLORMAP_JET)
+            self.pub_depth.publish(self.cv_bridge.cv2_to_imgmsg(frame_depth))
+            self.get_logger().info('Publishing Depth')
+
+            frame_color = inCamera.getCvFrame()
+            self.pub_color.publish(self.cv_bridge.cv2_to_imgmsg(frame_color))
+            self.get_logger().info('Publishing Color')
+
+            RotationVector = imuMessage.packets[-1].rotationVector
+            quat_msg = Quaternion(x=RotationVector.x, y=RotationVector.y, z=RotationVector.z, w=RotationVector.real)
+            self.pub_quat.publish(quat_msg)
+
 def main(args=None):
     rclpy.init(args=args)
-    depth_publisher = Depth_Frame_Pub()
+    publisher = OAK_D_Pro()
     try:
-        depth_publisher.run()
+        publisher.run()
     except KeyboardInterrupt:
         pass
     finally:
-        depth_publisher.destroy_node()
+        publisher.destroy_node()
         rclpy.shutdown()
 
 if __name__ == '__main__':
