12de1a37e1b93ef3dda48db8025d9ff2e2f275c2
ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2024-02-06 20:15:42 -0700

    rearranged everything

--- build/OAK_D_Pro/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/ubuntu/Desktop/Capstone-Autonomous-Driving/install/OAK_D_Pro'
--- build/cv_basics/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/ubuntu/Desktop/Capstone-Autonomous-Driving/install/cv_basics'
