99d71110063ba145932ad3074703ce14f30b052a
Devesh Nath <deveshnath101@gmail.com>
2024-02-28 20:26:34 -0700

    OAK D Pro IMU Msg Integration

--- ros2_ws/install/oak_d_pro/share/oak_d_pro/package.sh
+++ ros2_ws/install/oak_d_pro/share/oak_d_pro/package.sh
@@ -80,6 +80,7 @@ _colcon_package_sh_source_script() {
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/oak_d_pro/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/oak_d_pro/hook/ament_prefix_path.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/../../build/oak_d_pro/share/oak_d_pro/hook/pythonpath_develop.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
